53991d363e697ae873cde5c7d46dbcec9740d378
shalevc1098 <shalevc8901@gmail.com>
2024-07-01 23:05:59 +0300

    mod files

--- src/client/java/com/example/mixin/DynamicModelSwapper.java
+++ src/client/java/com/example/mixin/DynamicModelSwapper.java
@@ -33,7 +33,7 @@ public class DynamicModelSwapper {
         at=@At("HEAD"), cancellable=true)
     private void getDynamicModel(ItemStack stack, CallbackInfoReturnable<BakedModel> info){
         if(Registries.ITEM.getId(stack.getItem()).equals(itemIdentifier)) {
-            URLSprite urlSprite = new URLSprite("https://cdn.sstatic.net/Img/home/illo-home-hero.png?v=4718e8f857c5", Identifier.of("infinitecraft", "item/chatgpt"));
+            URLSprite urlSprite = new URLSprite("https://raw.githubusercontent.com/SamsTheNerd/cobblecards/main/common/src/main/resources/assets/cobblecards/textures/item/cobblecard.png", Identifier.of("infinitecraft", "item/chatgpt"));
             if (!urlSprite.getTextureId().equals(Identifier.of(""))) {
                 Identifier spriteIdentifier = urlSprite.getTextureId();
                 DynamicModelOverride override = new DynamicModelOverride(Identifier.of("infinitecraft", "chatgpt"), false, List.of(spriteIdentifier));
